05473624490db56bee6b86490e26102a5f673d69
nejdetkadir <nejdetkadir.550@gmail.com>
2020-10-13 21:04:04 +0300

    Create home page

--- app.js
+++ app.js
@@ -3,11 +3,13 @@ const express = require('express');
 const path = require('path');
 const cookieParser = require('cookie-parser');
 const logger = require('morgan');
+const session = require('express-session');
 const passport = require('passport');
 const dotenv = require('dotenv').config();
 
 const indexRouter = require('./routes/index');
 const authRouter = require('./routes/auth');
+const homeRouter = require('./routes/home');
 
 const app = express();
 const db = require('./helpers/db')();
@@ -23,9 +25,23 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 app.use(express.static(path.join(__dirname, 'bower_components')));
 
+// express-session
+app.use(session({
+  secret: process.env.SESSION_SECRET_KEY,
+  resave: false,
+  saveUninitialized: true,
+  cookie: {
+    //secure: true, // only use ssl connections
+    maxAge: 14 * 24 * 3600000 // you can change it.
+  }
+}));
+// passport.js
 app.use(passport.initialize());
+app.use(passport.session());
+// routes
 app.use('/', indexRouter);
 app.use('/auth', authRouter);
+app.use('/home', homeRouter);
 
 // catch 404 and forward to error handler
 app.use((req, res, next) => {
