44d159bb0028328aa77d0f2b4c4a2d24671270eb
nejdetkadir <nejdetkadir.550@gmail.com>
2020-10-13 21:20:34 +0300

    Show data of logged in user

--- routes/home.js
+++ routes/home.js
@@ -3,7 +3,7 @@ const router = express.Router();
 
 /* GET home page. */
 router.get('/', (req, res, next) => {
-  res.render('home', { title: 'Home' });
+  res.render('home', { title: 'Home', user: req.user });
 });
 
 module.exports = router;
--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,12 @@
 const express = require('express');
 const router = express.Router();
 
-/* GET home page. */
+/* GET login page. */
 router.get('/', (req, res, next) => {
-  res.render('index', { title: 'Express' });
+  if (!req.user)
+    res.render('index', { title: 'Login' });
+  else
+    res.redirect('/home');
 });
 
 module.exports = router;
